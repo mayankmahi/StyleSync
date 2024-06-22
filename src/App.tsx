f3e401a4dc3287e702122aed0024fa387d48489d
mayankmahi <mkmahi1198@gmail.com>
2024-06-22 11:36:13 +0530

    stylesync initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Style Sync
+    </div>
+  );
+}
+
+export default App;
